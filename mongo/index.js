b751e6b0acd417d1aac40887a3061ce5037437e3
akat32 <baduk32127@gmail.com>
2018-06-15 19:33:19 +0900

    fix schema index

--- mongo/index.js
+++ mongo/index.js
@@ -19,11 +19,6 @@ var UsersSchema = mongoose.Schema({
     time : {type : Date}
   }],
   profileImage : {type : String}, // 프로필 사진
-  userDiary : [{ // 유저 일기
-    title : {type : String, default : "제목"}, // 일기 제목
-    date : {type : Date}, // 일기 쓴 날짜
-    diary : {type : String} // 일기
-  }],
   mandalArtTheme : [{  // 만다라트 테마
     themaCode : {type : String} // 테마 코드 (랜덤스트링)
   }],
@@ -32,11 +27,19 @@ var UsersSchema = mongoose.Schema({
     goal : {type : String}, // 만다라트 목표
     mostWork : {type : String}, // 가장 많이 한 항목
     achievement : {type : Number, default : 0}, // 성취도
-    shape : {type : String}, // 모양
     startDay : {type : Date}, //시작일
-    isEnd : {type : Boolean, default : false}, // 이거 종료됨?
     endDay : {type : Date}, // 종료일
+    isEnd : {type : Boolean, default : false}, // 이거 종료됨?
     themaCode : {type : String, default : "base"}, // 테마
+    userDiary : [{ // 유저 일기
+      title : {type : String, default : "제목"}, // 일기 제목
+      date : {type : Date}, // 일기 쓴 날짜
+      diary : {type : String} // 일기
+    }],
+    mandalLog : [{
+      date : {type : Date}, // 로그 찍힌 날짜
+      index : {type : String} // 내용
+    }],
     middleMandalArt : [{ // 3 * 3 칸 짜리 만다라트 칸
       middleTitle : {type : String}, // 중간 칸 목표
       achievement : {type : Number, default : 0}, // 성취도
@@ -46,8 +49,21 @@ var UsersSchema = mongoose.Schema({
       }]
     }]
   }],
-  count : {type : Number, default : 0}, // 만다라트 갯수
-  maximumCount : {type : Number, default : 3} // 만다라트 최대 갯수
+  triangleMandalArt : [{ // 삼각형 만다라트
+    name : {type : String}, // 만다라트 이름
+    goal : {type : String}, // 만다라트 목표
+    isEnd : {type : String}, // 이거 끝남?
+    achievement : {type : Number, default : 0},
+    themaCode : {type : String, default : "base"}, //테마 코드
+    middleMandalArt : [{ // 중간 크기 만다라트
+      middleTitle : {type : String}, // 중간 칸 목표
+      achievement : {type : Number, default : 0}, // 중간 칸 성취도
+      smallMandalArt : [{ // 작은 칸
+        title : {type : String}, // 작은 칸 목표
+        achievement : {type : Number, default : 0} //작은 칸 성취도
+      }]
+    }]
+  }]
 });
 Users = mongoose.model('users', UsersSchema);
 
