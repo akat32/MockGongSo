5352b0173806b6526009fdbba1b30af015f07448
akat32 <baduk32127@gmail.com>
2018-08-28 22:06:38 +0900

    modi

---
+++ public/js/footer.js
@@ -0,0 +1,19 @@
+function goPage1(){
+  location.replace('/mandal')
+}
+
+function goPage2(){
+  location.replace('/mandalt')
+}
+
+function goPage3(){
+
+}
+
+function goPage4(){
+
+}
+
+function goHome(){
+  location.replace('/')
+}
