c98f162563137c98885f168ec6941fec4e5a5011
akat32 <baduk32127@gmail.com>
2018-09-02 15:14:32 +0900

    add mandalBox's event handler

--- public/js/mandal.js
+++ public/js/mandal.js
@@ -11,6 +11,11 @@ window.onload = async ()=>{
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
   var mandal2 = new Vue({
@@ -18,6 +23,11 @@ window.onload = async ()=>{
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
   var mandal3 = new Vue({
@@ -25,6 +35,11 @@ window.onload = async ()=>{
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
   var mandal4 = new Vue({
@@ -32,34 +47,59 @@ window.onload = async ()=>{
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
   var mandal5 = new Vue({
-    el: '.Box5',
-    data:{
-      isActive: false,
-      title : ''
-    }
-  })
-  var mandal6 = new Vue({
     el: '.Box6',
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
-  var mandal7 = new Vue({
+  var mandal6 = new Vue({
     el: '.Box7',
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
-  var mandal8 = new Vue({
+  var mandal7 = new Vue({
     el: '.Box8',
     data:{
       isActive: false,
       title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
+    }
+  })
+  var mandal8 = new Vue({
+    el: '.Box9',
+    data:{
+      isActive: false,
+      title : ''
+    },
+    methods:{
+      goDown : (event)=>{
+        alert('헤헷')
+      }
     }
   })
   var result = await axios.post('/getMandal',{some : "thing"});
@@ -74,13 +114,21 @@ window.onload = async ()=>{
   }
   else if(result.status == 200){
     mandal1.title = result.data.re.mandal[0].middleTitle;
+    if(result.data.re.mandal[0].middleTitle != '') mandal1.isActive = true;
     mandal2.title = result.data.re.mandal[1].middleTitle;
+    if(result.data.re.mandal[1].middleTitle != '') mandal2.isActive = true;
     mandal3.title = result.data.re.mandal[2].middleTitle;
+    if(result.data.re.mandal[2].middleTitle != '') mandal3.isActive = true;
     mandal4.title = result.data.re.mandal[3].middleTitle;
+    if(result.data.re.mandal[3].middleTitle != '') mandal4.isActive = true;
     mandal5.title = result.data.re.mandal[4].middleTitle;
+    if(result.data.re.mandal[4].middleTitle != '') mandal5.isActive = true;
     mandal6.title = result.data.re.mandal[5].middleTitle;
+    if(result.data.re.mandal[5].middleTitle != '') mandal6.isActive = true;
     mandal7.title = result.data.re.mandal[6].middleTitle;
+    if(result.data.re.mandal[6].middleTitle != '') mandal7.isActive = true;
     mandal8.title = result.data.re.mandal[7].middleTitle;
+    if(result.data.re.mandal[7].middleTitle != '') mandal8.isActive = true;
     mandalTitle.title = result.data.re.title;
     mandalTitle.achievement = result.data.re.achievement;
   }
