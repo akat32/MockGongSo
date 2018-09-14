516ece274c416cb89df632970423cb471467521d
akat32 <baduk32127@gmail.com>
2018-09-14 20:58:20 +0900

    add getDiary

--- routes/diary/diary.js
+++ routes/diary/diary.js
@@ -11,7 +11,38 @@ function diary(app, passport, Users, rndstring){
     if(!result.ok) return res.status(500).json({message : "ERR!"})
     return res.status(200).json({message : "success!"})
   })
-  .post('/getDiary', async (req,res)=>{
-
+  .post('/getDiary/app', async (req,res)=>{
+    var user = await Users.findOne({token : req.body.token})
+    if(!user) return res.status(404).json({message : "User Not Found!"})
+    var diary = user.userDiary;
+    var re = [];
+    for(var i = 0; diary[i] != null; i++){
+      console.log(diary[i])
+      var chk = 0;
+      var new_diary = {
+        date : "",
+        diary : [{
+          index : ""
+        }]
+      }
+      for (var j = 0; re[j] != null; j++){
+        if(diary[i].date === re[j].date){
+          var diaryIndex = { index : diary[i].index }
+          re[j].diary.push(diaryIndex);
+          chk = 1;
+        }
+      }
+      if(!chk){
+        new_diary.date = diary[i].date;
+        new_diary.diary[0].index = diary[i].index
+        re.push(new_diary);
+      }
+    }
+    function com(a, b){
+      if(a.date == b.date){ return 0 }
+      return a.date > b.date ? 1 : -1;
+    }
+    re.sort(com);
+    res.status(200).json({re : re});
   })
 }
